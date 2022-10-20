7e38d7dfba51c95af993ec899fb8d7bfd2b92bd1
mohammadshahzeb0012 <mohammadshahzeb0012@gmail.com>
2022-10-20 19:34:35 +0530

    portfolio site

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import logo from './logo.svg';
+import './App.css';
+import NavBar from './components/NavBar';
+import Home from './components/Home';
+import SocialLinks from './components/SocialLinks';
+import About from './components/About';
+import Portfolio from './components/Portfolio';
+import Experiance from './components/Experiance';
+import Contact from './components/Contact';
+
+
+
+function App() {
+  return (
+    <><div>
+  <NavBar />
+  <Home />
+  <About />
+  <Portfolio />
+  <Experiance />
+  <Contact />
+  <SocialLinks />
+
+    </div></>
+  );
+}
+
+export default App;
